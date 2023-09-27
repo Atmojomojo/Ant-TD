b4d974cf74197fef619c10f5ec077eddfd3ed883
TamaraVeltman <124800131+TamaraVeltman@users.noreply.github.com>
2023-09-27 13:26:00 +0200

    UI/UX
    Resolution script
    point down ui

--- Assets/Tamara/Scripts/Resolution.cs
+++ Assets/Tamara/Scripts/Resolution.cs
@@ -1,18 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Resolution : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
