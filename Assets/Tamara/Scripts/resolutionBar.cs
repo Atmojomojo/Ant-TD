bd5e4f1bda39529623f064de0550dbca36431a50
TamaraVeltman <124800131+TamaraVeltman@users.noreply.github.com>
2023-09-28 16:57:19 +0200

    UI/UX
    Canvas/panels.
    Resolution script.
    Level select.

---
+++ Assets/Tamara/Scripts/resolutionBar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using TMPro;
+public class resolutionBar : MonoBehaviour
+{
+    public TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+        }
+
+        resolutionDropdown.AddOptions(options);
+
+    }
+}
