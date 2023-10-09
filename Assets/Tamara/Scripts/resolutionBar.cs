b797083692f0f5498ebe4e9f9eff36796c4dd95d
TamaraVeltman <124800131+TamaraVeltman@users.noreply.github.com>
2023-10-09 15:32:41 +0200

    UI/UX
    Resolutie script
    Tower select/shop ui

--- Assets/Tamara/Scripts/resolutionBar.cs
+++ Assets/Tamara/Scripts/resolutionBar.cs
@@ -15,41 +15,56 @@ public class ResolutionBar : MonoBehaviour
     Resolution[] resolutions;
     void Start()
     {
-        resolutions = Screen.resolutions;
+        //resolutions = //Screen.resolutions;
+        resolutions = new Resolution[]
+        {
+            new Resolution(),
+            new Resolution(),
+            new Resolution(),
+            new Resolution(),
+            new Resolution()
+        };
+        
+        resolutions[0].width = 1280;
+        resolutions[0].height = 720;
+        resolutions[1].width = 1440;
+        resolutions[1].height = 900;
+        resolutions[2].width = 1366;
+        resolutions[2].height = 720;
+        resolutions[3].width = 1920;
+        resolutions[3].height = 1080;
+        resolutions[4].width = 2560;
+        resolutions[4].height = 1440;
 
         resolutionDropdown.ClearOptions();
 
         List<string> options = new List<string>();
 
-        int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + "x" + resolutions[i].height;
             options.Add(option);
-
-            if (resolutions[i].width == Screen.resolutions[currentResolutionIndex].width &&
-                resolutions[i].height == Screen.resolutions[currentResolutionIndex].height)
-            {
-                currentResolutionIndex = i;
-            }
-
         }
 
         resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.value = resolutions.Length - 1;
         resolutionDropdown.RefreshShownValue();
+
+        resolutionDropdown.value = resolutions.Length - 1;
+        Screen.fullScreen = true;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+
+    Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
 
     }
 
-        public void SetResolution(int resolutionIndex)
-        {
-         Resolution resolution = resolutions[resolutionIndex];
-
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-
-        }
-   
-       
-        
+    public void SetRes ()
+    {
+        SetResolution(resolutionDropdown.value);
+    }
 
 }
