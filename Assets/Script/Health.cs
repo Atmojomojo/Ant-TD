a07c5a0bfbab1a8c62181857a74538a26f224c4f
Atmojomojo <mairo@samajo.nl>
2023-10-10 22:28:08 +0200

    Deathscreen aanroepen
    Deathscreen popt in scherm als je dood gaat
    
    De text is een (grappige) placeholder

--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -7,6 +7,7 @@ public class Health : MonoBehaviour
 {
     public int health = 20;
     public TMP_Text healthText;
+    public GameObject deathScreen;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,7 @@ public class Health : MonoBehaviour
         healthText.SetText(health.ToString());
         if (health <= 0)
         {
+            deathScreen.SetActive(true);
             Debug.Log("Game Over You Lost!!!");
         }
     }
