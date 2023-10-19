c176f9951645ae3454bea9e8b314bec0ee57c0cc
Atmojomojo <mairo@samajo.nl>
2023-10-19 10:58:45 +0200

    Notenschieter fixed
    notenschieter schoot naar beneden ipv vooruit

--- Assets/Script/Towers/Noten/Zaadje.cs
+++ Assets/Script/Towers/Noten/Zaadje.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Noot: MonoBehaviour
+public class Zaadje : MonoBehaviour
 {
-    public float bulletSpeed;
+    public float bulletSpeed, gravity;
     public float damage;
     private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.velocity = transform.forward * bulletSpeed; // Set the initial forward velocity
+        rb.velocity = -transform.up * bulletSpeed; // Set the initial forward velocity
+    }
+
+    void Update()
+    {
+        // Simulate gravity (bullet drop) by applying a force in the downward direction
+        rb.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
     }
 
     void OnCollisionEnter(Collision collision)
@@ -28,5 +34,3 @@ public class Noot: MonoBehaviour
         Destroy(gameObject);
     }
 }
-
-
