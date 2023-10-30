5ea6658d51d5ca3c0941b94d201caa55d6f49b6e
TamaraVeltman <124800131+TamaraVeltman@users.noreply.github.com>
2023-10-30 15:03:48 +0100

    UI/UX
    BlurInGame
    BlurInMenu
    
    FireTower en Bessenschieter toegevoegd

--- Assets/Tamara/Scripts/Timepause.cs
+++ Assets/Tamara/Scripts/Timepause.cs
@@ -1,28 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
+using UnityEngine.Profiling;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 
 public class TimePause : MonoBehaviour
 {
     
-        private bool isPaused = false;
+    private bool isPaused = false;
+    public VolumeProfile profile;
+    private DepthOfField dof;
 
-        public void PauseTime()
+    public void PauseTime()
+    {
+        if (!isPaused)
         {
-            if (!isPaused)
-            {
-                Time.timeScale = 0;
-                isPaused = true;
-            }
+            Time.timeScale = 0;
+            isPaused = true;
+            //profile.TryGet<DepthOfField>(out dof);
+            //{
+            //    dof.gaussianStart.value = 0;
+            //    dof.gaussianEnd.value = 0;
+            //}
         }
+    }
 
-        public void ResumeTime()
+    public void BlurInMenu()
+    {
+        profile.TryGet<DepthOfField>(out dof);
         {
-            if (isPaused)
-            {
-                Time.timeScale = 1;
-                isPaused = false;
-            }
+            dof.gaussianStart.value = 0;
+            dof.gaussianEnd.value = 0;
         }
-    
+    }
+
+    public void ResumeTime()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+            //profile.TryGet<DepthOfField>(out dof);
+            //{
+            //    dof.gaussianStart.value = 62;
+            //    dof.gaussianEnd.value = 90;
+            //}
+        }
+    }
+
+    public void BlurInGame()
+    {
+        profile.TryGet<DepthOfField>(out dof);
+        {
+            dof.gaussianStart.value = 62;
+            dof.gaussianEnd.value = 90;
+        }
+    }
 }
+
+
+
+
