77554a4b1dfdcdbbea87cb04b18fe1e4425f3cd4
TamaraVeltman <124800131+TamaraVeltman@users.noreply.github.com>
2023-11-04 00:20:45 +0100

    UI/UX, lighting
    Lighting map 1 gebaked
    Mountedturretpopup gedeeltelijk af
    Script turretswitch aangepast

--- Assets/Script/Towers/MountedTurret/TurretSwitch.cs
+++ Assets/Script/Towers/MountedTurret/TurretSwitch.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class TurretSwitch : MonoBehaviour
 {
@@ -12,6 +13,22 @@ public class TurretSwitch : MonoBehaviour
     public Camera main, turret;
     public MountedTurret mountedTurret;
     public MountedTurretShoot shoot;
+
+
+
+    public GameObject inGameUI;
+    public GameObject MountedturretShootUI;
+    public GameObject MountedturretLeaveUI;
+
+
+    public float hideShootUIDelayInSeconds = 4f;
+    public float showLeaveUIDelayInSeconds = 6f;
+    public float hideLeaveUIDelayInSeconds = 3f;
+
+    private bool isShootUIHidden = false;
+    private bool hasEnteredTurret = false;
+
+
     public void SwitchToTurret()
     {
         if (turretActive == false)
@@ -24,6 +41,27 @@ public class TurretSwitch : MonoBehaviour
             playerInput.defaultActionMap = "MountedTurret";
             playerInput.camera = turret;
             turretActive = true;
+
+            inGameUI.SetActive(true);
+            Debug.Log("MountedturretShootUI");
+            MountedturretShootUI.SetActive(true);
+
+            if (!hasEnteredTurret)
+            {
+                
+                hasEnteredTurret = true;
+
+                if (showLeaveUIDelayInSeconds > 0)
+                {
+                    Invoke("ShowMountedturretLeaveUI", showLeaveUIDelayInSeconds);
+                }
+            }
+
+            if (hideShootUIDelayInSeconds > 0)
+            {
+                Invoke("HideMountedturretShootUI", hideShootUIDelayInSeconds);
+                
+            }
         }
         else
         {
@@ -35,7 +73,51 @@ public class TurretSwitch : MonoBehaviour
             playerInput.defaultActionMap = "Player";
             playerInput.camera = main;
             turretActive = false;
+
+            
+            inGameUI.SetActive(false);
+            MountedturretShootUI.SetActive(false);
+            MountedturretLeaveUI.SetActive(false);
+
+            if (hideLeaveUIDelayInSeconds > 0)
+            {
+                CancelInvoke("ShowMountedturretLeaveUI");
+                Invoke("HideMountedturretLeaveUI", hideLeaveUIDelayInSeconds);
+            }
+
+            
+            hasEnteredTurret = false;
+            
+
+        }
+
+    }
+    void HideMountedturretShootUI()
+    {
+        MountedturretShootUI.SetActive(false);
+        isShootUIHidden = true;
+
+        if (showLeaveUIDelayInSeconds > 0)
+        {
+            Invoke("ShowMountedturretLeaveUI", showLeaveUIDelayInSeconds);
         }
     }
-   
+
+    void ShowMountedturretLeaveUI()
+    {
+        MountedturretLeaveUI.SetActive(true);
+
+        if (hideLeaveUIDelayInSeconds > 0)
+        {
+            Invoke("HideMountedturretLeaveUI", hideLeaveUIDelayInSeconds);
+        }
+    }
+
+    void HideMountedturretLeaveUI()
+    {
+        MountedturretLeaveUI.SetActive(false);
+    }
 }
+
+
+
