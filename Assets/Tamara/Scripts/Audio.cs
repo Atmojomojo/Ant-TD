72db54f11f68efbd29938f76a5d38e2fb29fb037
TamaraVeltman <124800131+TamaraVeltman@users.noreply.github.com>
2023-11-08 11:38:24 +0100

    Audio
    Bugfix audio

--- Assets/Tamara/Scripts/Audio.cs
+++ Assets/Tamara/Scripts/Audio.cs
@@ -16,52 +16,56 @@ public class Audio : MonoBehaviour
 
     void Start()
     {
-        if (!PlayerPrefs.HasKey("musicVolume"))
-        {
-            PlayerPrefs.SetFloat("musicVolume", 0.8f);
-        }
+        //if (!PlayerPrefs.HasKey("musicVolume"))
+        //{
+        //    PlayerPrefs.SetFloat("musicVolume", 0.8f);
+        //}
 
-        if (!PlayerPrefs.HasKey("sfxVolume"))
-        {
-            PlayerPrefs.SetFloat("sfxVolume", 0.8f);
-        }
+        //if (!PlayerPrefs.HasKey("sfxVolume"))
+        //{
+        //    PlayerPrefs.SetFloat("sfxVolume", 0.8f);
+        //}
 
-        if (!PlayerPrefs.HasKey("allsoundsVolume"))
-        {
-            PlayerPrefs.SetFloat("allsoundsVolume", 0.8f);
-        }
+        //if (!PlayerPrefs.HasKey("allsoundsVolume"))
+        //{
+        //    PlayerPrefs.SetFloat("allsoundsVolume", 0.8f);
+        //}
 
-        Load();
-    }
-
-    public void SetSFXSlider(float newVolume)
-    {
-        sfx.audioMixer.SetFloat("Volume", (newVolume == 0) ? -80f : Mathf.Log10(newVolume) * 20);
-        PlayerPrefs.SetFloat("sfxVolume", sfxSlider.value);
-    }
-
-    public void SetMusicSlider(float newVolume)
-    {
-        music.audioMixer.SetFloat("Volume", (newVolume == 0) ? -80f : Mathf.Log10(newVolume) * 20);
-        PlayerPrefs.SetFloat("musicVolume", musicSlider.value);
-    }
-
-    public void SetallsoundsSlider(float newVolume)
-    {
-        allsounds.SetFloat("Volume", (newVolume == 0) ? -80f : Mathf.Log10(newVolume) * 20);
-        PlayerPrefs.SetFloat("allsoundsVolume", allsoundsSlider.value);
-    }
-
-    private void Load()
-    {
+        ////Load();
+        ///
         musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
-        music.audioMixer.SetFloat("Volume", musicSlider.value);
-
         sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
-        sfx.audioMixer.SetFloat("Volume", sfxSlider.value);
-
         allsoundsSlider.value = PlayerPrefs.GetFloat("allsoundsVolume");
-        allsounds.SetFloat("Volume", allsoundsSlider.value);
     }
+
+    public void SetSFXSlider(float sliderValue)
+    {
+        sfx.audioMixer.SetFloat("VolumeSFX",  Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("sfxVolume", sliderValue);
+    }
+
+    public void SetMusicSlider(float sliderValue)
+    {
+        music.audioMixer.SetFloat("VolumeMusic", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("musicVolume", sliderValue);
+    }
+
+    public void SetallsoundsSlider(float sliderValue)
+    {
+        allsounds.SetFloat("VolumeMaster", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("allsoundsVolume", sliderValue);
+    }
+
+    //private void Load()
+    //{
+    //    musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    //    music.audioMixer.SetFloat("Volume", musicSlider.value);
+
+    //    sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+    //    sfx.audioMixer.SetFloat("Volume", sfxSlider.value);
+
+    //    allsoundsSlider.value = PlayerPrefs.GetFloat("allsoundsVolume");
+    //    allsounds.SetFloat("Volume", allsoundsSlider.value);
+    //}
 }
 
